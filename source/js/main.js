aba3f9484432f78c0a0655b10401fbf86ed90767
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-04-07 23:39:12 +0300

    feat(expander): добавляет скрипт скрытия контента

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,10 @@
+import initExpander from './modules/expander';
+
 window.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line no-console
   console.log('DOM Content Loaded');
 
   window.addEventListener('load', () => {
-    // eslint-disable-next-line no-console
-    console.log('load');
+    initExpander();
   });
 });
---
+++ source/js/modules/expander.js
@@ -0,0 +1,43 @@
+const containersToExpand = document.querySelectorAll('[data-expander]');
+
+const buttonClickHandler = (evt) => {
+  const {target} = evt;
+
+  if (!target.closest('[data-expander]')) {
+    return;
+  }
+
+  const container = target.closest('[data-expander]');
+  const content = container.querySelector('[data-expander-content]');
+  const button = container.querySelector('[data-expander-btn]');
+  const buttonText = button.querySelector('[data-expander-btn-text]');
+
+  evt.preventDefault();
+
+  if (button.getAttribute('aria-expanded') === 'true') {
+    content.style.maxHeight = null;
+    button.setAttribute('aria-expanded', 'false');
+    buttonText.textContent = content.dataset.expanderContent === 'text'
+      ? 'Читать далее'
+      : 'Показать всё';
+
+    return;
+  }
+
+  content.style.maxHeight = `${content.scrollHeight}px`;
+  button.setAttribute('aria-expanded', 'true');
+  buttonText.textContent = 'Скрыть';
+};
+
+const activateExpander = (container) => {
+  const button = container.querySelector('[data-expander-btn]');
+  button.addEventListener('click', buttonClickHandler);
+};
+
+const initExpander = () => {
+  if (containersToExpand.length) {
+    containersToExpand.forEach(activateExpander);
+  }
+};
+
+export default initExpander;
