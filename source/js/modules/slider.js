492defbcce160ecf43f3c25b9a68adddbeb8d75e
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-05-08 23:05:07 +0300

    fix(swiper): исправляет инициализацию слайдера

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,21 +1,60 @@
-const slider = document.querySelector('.page-nav');
+const sliders = document.querySelectorAll('[data-swiper]');
 
-const config = {
-  wrapperClass: 'page-nav__list',
-  slideClass: 'page-nav__item',
-  slidesPerView: 'auto',
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
-  a11y: {
-    slideLabelMessage: 'Слайд {{index}} из {{slidesLength}}',
-  },
-};
+const createConfig = (dataAttrValue, configType) => ((configType && configType === 'simple')
+  ? {
+    wrapperClass: `${dataAttrValue}__list`,
+    slideClass: `${dataAttrValue}__item`,
+    slidesPerView: 'auto',
+    a11y: {
+      slideLabelMessage: 'Пункт меню {{index}} из {{slidesLength}}',
+    },
+  }
+  : {
+    wrapperClass: `${dataAttrValue}__list`,
+    slideClass: `${dataAttrValue}__item`,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    freeMode: {
+      enabled: true,
+      momentumBounce: false,
+    },
+    loop: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    speed: 5000,
+    createElements: true,
+    pagination: true,
+    a11y: {
+      slideLabelMessage: 'Слайд {{index}} из {{slidesLength}}',
+    },
+    breakpoints: {
+      0: {
+        spaceBetween: 16,
+        enabled: true,
+      },
+      360: {
+        spaceBetween: 24,
+        enabled: true,
+      },
+      768: {
+        enabled: false,
+        spaceBetween: 0,
+      },
+    },
+  });
 
 const initSlider = () => {
-  // eslint-disable-next-line no-undef, no-unused-vars
-  const swiper = new Swiper(slider, config);
+  sliders.forEach((slider) => {
+    const config = createConfig(
+      slider.dataset.swiper,
+      slider.dataset.swiperConfig,
+    );
+
+    // eslint-disable-next-line no-unused-vars
+    const swiper = new window.Swiper(slider, config);
+  });
 };
 
 export default initSlider;
