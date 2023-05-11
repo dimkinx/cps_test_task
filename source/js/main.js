7b3fb837b01217cc091f62b17bc2b9914b995eb0
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2023-05-11 22:25:32 +0300

    feat: добавляет фикс высоты вьюпорта для iOS

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,10 @@
+import iosVhFix from './utils/ios-vh-fix';
 import initSliders from './modules/slider';
 import initMenu from './modules/menu';
 import initExpander from './modules/expander';
 
 window.addEventListener('DOMContentLoaded', () => {
+  iosVhFix();
   initSliders();
 
   window.addEventListener('load', () => {
---
+++ source/js/utils/ios-checker.js
@@ -0,0 +1,10 @@
+const iosChecker = () => [
+  'iPad Simulator',
+  'iPhone Simulator',
+  'iPod Simulator',
+  'iPad',
+  'iPhone',
+  'iPod',
+].includes(navigator.platform) || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+
+export default iosChecker;
